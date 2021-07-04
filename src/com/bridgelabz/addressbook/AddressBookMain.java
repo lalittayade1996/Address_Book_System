432364a46b86941b7a4d1b47dda61283b1e91a44
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-07-04 19:08:09 +0530

    Added : Address Book Information

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,7 +1,27 @@
 package com.bridgelabz.addressbook;
 
+class Contacts {
+	String fName, lName, address, city, state;
+	Long zipCode, phoneNum;
+	String email;
+
+	Contacts(String fName, String lName, String address, String city, String state, long zipCode, long phoneNum,
+			String email) {
+		this.fName = fName;
+		this.lName = lName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNum = phoneNum;
+		this.email = email;
+	}
+}
+
 public class AddressBookMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Address Book Program");
+
+	public static void main(String args[]) {
+
+		System.out.println("Welcome to Address Book");
 	}
 }
